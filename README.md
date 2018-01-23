fccc7ed28ab824ddeb690c0f0ae74c68dc639ccc
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-01-23 19:50:56 +0300

    Ready to add functionality

---
+++ README.md
@@ -0,0 +1,16 @@
+# Nightlife coordination app
+
+You can view all bars in your area
+
+As an authenticated user you can: 
+    - add yourself to a bar to indicate you are going there tonight
+    - remove yourself from a bar if you no longer want to go there
+    
+When you login you should not have to search again
+
+
+Created by using [Clementine.js](https://github.com/johnstonbl01/clementinejs-fcc.git) boilerplate 
+
+## Clementine License
+
+MIT License. [Click here for more information.](LICENSE.md)
---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const routes = require('./app/routes/index.js');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI, {useMongoClient: true});
+mongoose.Promise = global.Promise;
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+const port = process.env.PORT || 8080;
+app.listen(port,  () => {
+	console.log(`Node.js listening on port ${port} ...`);
+});
