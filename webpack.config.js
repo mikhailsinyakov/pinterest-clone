54851f7a8c374eb30950840a56be15b0dda4ceb2
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-12-23 14:45:26 +0300

    Updated packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: __dirname + '/app/src/index.js',
+    entry: './app/src/index.js',
     output: {
         path: __dirname + '/public/',
         filename: 'script.js'
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react']
-            }
+            use: 'babel-loader'
         }]
     },
     mode: 'development'
